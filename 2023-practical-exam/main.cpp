820b8aac9f75897b256c0780a507f89d6dff3488
Nkubito Pacis <pacisnkubito@gmail.com>
2023-06-21 11:26:04 +0200

    2023-practical-exam

---
+++ 2023-practical-exam/main.cpp
@@ -0,0 +1,196 @@
+#include <iostream>     // Input/output stream operations
+#include <string>       // String operations
+#include <sstream>      // String stream operations
+#include <fstream>      // File input/output operations
+#include <vector>       // Vector container
+#include <algorithm>    // Algorithms
+#include <iomanip>      // Input/output manipulators
+
+#include "utilities.cpp"
+
+using namespace std;
+
+string ITEMS_FILE = "items.csv";
+
+struct Item
+{
+    string item_id;
+    string item_name;
+    string item_quantity;
+    string item_registration_date;
+};
+
+// Function to compare items based on item_name for sorting
+bool compareItems(const Item &item1, const Item &item2)
+{
+    return item1.item_name < item2.item_name;
+}
+
+// Function to list all items
+void listItems()
+{
+    ifstream file = openInputFile(ITEMS_FILE);
+    string item;
+    vector<Item> items;
+
+    // Read items from file and store them in a vector
+    while (getline(file, item))
+    {
+        vector<string> row = splitString(item, ',');
+
+        Item newItem;
+        newItem.item_id = row[0];
+        newItem.item_name = row[1];
+        newItem.item_quantity = row[2];
+        newItem.item_registration_date = row[3];
+
+        items.push_back(newItem);
+    }
+
+    // Check if there are any items
+    if (items.empty())
+    {
+        cout << "No items found" << endl;
+    }
+    else
+    {
+        // Sort the items based on item_name
+        sort(items.begin(), items.end(), compareItems);
+
+        // Display each item's details
+        for (const auto &item : items)
+        {
+            cout << "Item ID: " << setw(2) << item.item_id << "            "
+                 << "Item Name: " << setw(10) << item.item_name << "                "
+                 << "Quantity: " << setw(2) << item.item_quantity << "      "
+                 << "Reg Date: " << item.item_registration_date << endl
+                 << endl;
+        }
+    }
+
+    file.close();
+}
+
+// Function to add a new item
+void addItem(string &item_id, string &item_name, string &item_quantity, string &item_registration_date)
+{
+
+    // Validate the input data format
+    if (!isNumeric(item_id) || !isNumeric(item_quantity) || !isValidDateFormat(item_registration_date))
+    {
+        cout << "Invalid data format. Please enter the following format: <item_id as a number> <item_name> <item_quantity as a number> <item_registration_date as date in YYYY-mm-dd>" << endl;
+        return;
+    }
+
+    // Open the items file for writing
+    ofstream file = openOutputFile(ITEMS_FILE);
+    // Open the items file for reading to check for duplicates
+    ifstream inputFile = openInputFile(ITEMS_FILE);
+
+    // Check if the item with the given item_id already exists
+    if (!checkDuplicate(inputFile, item_id, 0))
+    {
+        // Write the new item to the file
+        file << item_id << "," << item_name << "," << item_quantity << "," << item_registration_date << endl;
+        cout << "Item is added successfully." << endl;
+    }
+    else
+    {
+        cout << "Item with id: " << item_id << " already exists." << endl;
+    }
+
+    file.close();
+    inputFile.close();
+}
+
+// Function to display the help menu
+void help()
+{
+    cout << " ===========================================================================" << endl;
+    cout << " *                            Command syntaxes                              *" << endl;
+    cout << " ===========================================================================" << endl;
+    cout << " itemadd <item_id> <item_name> <item_quantity> <item_registration_date>" << endl;
+    cout << " itemslist" << endl;
+    cout << " help                                      :Prints user manual" << endl;
+    cout << " exit                                      :Exit the program" << endl;
+    cout << " =============================================================================" << endl;
+}
+
+// Function to display an invalid command message
+void invalidCommand(string correctCommand)
+{
+    cout << "Invalid command. Usage: " << correctCommand << endl;
+}
+
+// Function to process user commands
+int processCommands()
+{
+    cout << "Need help? Type 'help' then press Enter key." << endl;
+
+    string input;
+
+    string command;
+    do
+    {
+        cout << "Console >";
+        getline(cin, input);
+
+        istringstream iss(input);
+        iss >> command;
+        // Convert the command to lowercase for case-insensitive comparison
+        for (size_t i = 0; i < command.length(); i++)
+        {
+            command[i] = tolower(command[i]);
+        }
+        // Process the command
+        if (command == "itemslist")
+        {
+            listItems();
+        }
+        else if (command == "itemadd")
+        {
+            string item_id, item_name, item_quantity, item_registration_date;
+            iss >> item_id >> item_name >> item_quantity >> item_registration_date;
+
+            if (item_id.empty() || item_name.empty() || item_quantity.empty() || item_registration_date.empty())
+            {
+                invalidCommand("itemadd <item_id> <item_name> <item_quantity> <item_registration_date>");
+            }
+            else
+            {
+                addItem(item_id, item_name, item_quantity, item_registration_date);
+            }
+        }
+
+        else if (command == "help")
+        {
+            help();
+        }
+        else
+        {
+            // Invalid command entered
+            if (command != "exit")
+            {
+                cout << "Invalid command. Below is the help option to provide you list of commands." << endl;
+                help();
+            }
+        }
+    } while (command != "exit");
+
+    return 0;
+}
+
+int main()
+{
+    cout << " ==================================================== " << endl;
+    cout << "*     Welcome to RCA Inventory System!     *" << endl;
+    cout << "*  **************************************** *" << endl;
+    cout << "*                           *" << endl;
+    cout << "* It is developed by 'NKUBITO Pacis' as practical *" << endl;
+    cout << "* evaluation for the end of Year 3.               *" << endl;
+    cout << " ====================================================" << endl;
+
+    // Start processing user commands
+    processCommands();
+    return 0;
+}
---
+++ 2023-practical-exam/utilities.cpp
@@ -0,0 +1,94 @@
+#include <iostream> // Input/output stream operations
+#include <fstream>  // String stream operations
+#include <vector>   // Vector container
+#include <sstream>  // String stream operations
+#include <regex>    //Regular expressions
+
+using namespace std;
+
+bool isValidDateFormat(const string &date)
+{
+    // Regex pattern to match the date format "YYYY-MM-DD"
+    regex pattern("\\d{4}-(0[1-9]|1[0-2])-\\d{2}");
+
+    if (regex_match(date, pattern))
+    {
+        // Extract month from the date string
+        int month = stoi(date.substr(5, 2));
+
+        // Check if the month is greater than 12
+        if (month > 12)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+bool isNumeric(const string &str)
+{
+    // Regex pattern to match numeric characters
+    regex pattern("\\d+");
+
+    return regex_match(str, pattern);
+}
+
+ofstream openOutputFile(string &filename, const string &mode = "a")
+{
+    ofstream file;
+    if (mode == "a")
+    {
+        file.open(filename, ios::app);
+    }
+    else
+    {
+        file.open(filename, ios::out);
+    }
+
+    if (!file.is_open())
+    {
+        cout << "Failed to open file " << filename << endl;
+    }
+    return file;
+}
+
+// open file for reading
+ifstream openInputFile(string &filename)
+{
+    ifstream file(filename);
+
+    if (!file.is_open())
+    {
+        cout << "Failed to open file " << filename << endl;
+    }
+    return file;
+}
+
+vector<string> splitString(const string &input, char delimiter)
+{
+    vector<string> tokens;
+    stringstream ss(input);
+    string token;
+    while (getline(ss, token, delimiter))
+    {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+bool checkDuplicate(ifstream &file, const string &keyword, const int pos)
+{
+    string line;
+    while (getline(file, line))
+    {
+        vector<string> row = splitString(line, ',');
+        if (row.size() > pos && row[pos] == keyword)
+        {
+            return true;
+        }
+    }
+    return false;
+}
